c6e5ce0ba90c24b72d48d36cd8d23fe76a0fa8a9
jasontcrabtree <jasontcrabtree@gmail.com>
2023-12-04 22:04:52 +1300

    local build works, push to build

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,107 +1,114 @@
-import { useRouter } from 'next/router';
-import ErrorPage from 'next/error';
-import Head from 'next/head';
+// import { useRouter } from 'next/router';
+// import ErrorPage from 'next/error';
 
-import { getPostBySlug, getAllPosts } from '../../lib/api';
-import markdownToHtml from '../../lib/markdownToHtml';
+// import { getPostBySlug, getAllPosts } from '../../lib/api';
+// import markdownToHtml from '../../lib/markdownToHtml';
 
-// import Container from '../../components/container';
-import PostBody from '../../components/post-body';
-// import Header from '../../components/header';
-// import PostHeader from '../../components/post-header';
-// import Layout from '../../components/layout';
-// import PostTitle from '../../components/post-title';
-// import { CMS_NAME } from '../../lib/constants';
-// import type PostType from '../../interfaces/post';
+// // import Container from '../../components/container';
+// import PostBody from '../../components/post-body';
+// // import Header from '../../components/header';
+// // import PostHeader from '../../components/post-header';
+// // import Layout from '../../components/layout';
+// // import PostTitle from '../../components/post-title';
+// // import { CMS_NAME } from '../../lib/constants';
+// // import type PostType from '../../interfaces/post';
 
-// type Props = {
-//   post: PostType;
-//   morePosts: PostType[];
-//   preview?: boolean;
+// // type Props = {
+// //   post: PostType;
+// //   morePosts: PostType[];
+// //   preview?: boolean;
+// // };
+
+// export default function Post({ post, morePosts, preview }: any) {
+//   const router = useRouter();
+//   const title = `${post.title} | Next.js Blog Example with`;
+
+//   if (!router.isFallback && !post?.slug) {
+//     return <ErrorPage statusCode={404} />;
+//   }
+
+//   //   return (
+//   //     <Layout preview={preview}>
+//   //       <Container>
+//   //         <Header />
+//   //         {router.isFallback ? (
+//   //           <PostTitle>Loading…</PostTitle>
+//   //         ) : (
+//   //           <>
+//   //             <article className="mb-32">
+//   //               <Head>
+//   //                 <title>{title}</title>
+//   //                 <meta property="og:image" content={post.ogImage.url} />
+//   //               </Head>
+//   //               <PostHeader
+//   //                 title={post.title}
+//   //                 coverImage={post.coverImage}
+//   //                 date={post.date}
+//   //                 author={post.author}
+//   //               />
+//   //               <PostBody content={post.content} />
+//   //             </article>
+//   //           </>
+//   //         )}
+//   //       </Container>
+//   //     </Layout>
+//   //   );
+
+//   return (
+//     <div>
+//       <h1>hi</h1>
+//       <PostBody content={post.content} />
+//     </div>
+//   );
+// }
+
+// type Params = {
+//   params: {
+//     slug: string;
+//   };
 // };
 
-export default function Post({ post, morePosts, preview }: any) {
-  const router = useRouter();
-  const title = `${post.title} | Next.js Blog Example with`;
+// export async function getStaticProps({ params }: Params) {
+//   const post = getPostBySlug(params.slug, [
+//     'title',
+//     'date',
+//     'slug',
+//     'author',
+//     'content',
+//     'ogImage',
+//     'coverImage',
+//   ]);
+//   const content = await markdownToHtml(post.content || '');
 
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />;
-  }
+//   return {
+//     props: {
+//       post: {
+//         ...post,
+//         content,
+//       },
+//     },
+//   };
+// }
 
-  //   return (
-  //     <Layout preview={preview}>
-  //       <Container>
-  //         <Header />
-  //         {router.isFallback ? (
-  //           <PostTitle>Loading…</PostTitle>
-  //         ) : (
-  //           <>
-  //             <article className="mb-32">
-  //               <Head>
-  //                 <title>{title}</title>
-  //                 <meta property="og:image" content={post.ogImage.url} />
-  //               </Head>
-  //               <PostHeader
-  //                 title={post.title}
-  //                 coverImage={post.coverImage}
-  //                 date={post.date}
-  //                 author={post.author}
-  //               />
-  //               <PostBody content={post.content} />
-  //             </article>
-  //           </>
-  //         )}
-  //       </Container>
-  //     </Layout>
-  //   );
+// export async function getStaticPaths() {
+//   const posts = getAllPosts(['slug']);
 
+//   return {
+//     paths: posts.map(post => {
+//       return {
+//         params: {
+//           slug: post.slug,
+//         },
+//       };
+//     }),
+//     fallback: false,
+//   };
+// }
+
+export default function BlogPlaceholder() {
   return (
     <div>
-      <h1>hi</h1>
-      <PostBody content={post.content} />
+      <h1>Coming Soon</h1>
     </div>
   );
-}
-
-type Params = {
-  params: {
-    slug: string;
-  };
-};
-
-export async function getStaticProps({ params }: Params) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ]);
-  const content = await markdownToHtml(post.content || '');
-
-  return {
-    props: {
-      post: {
-        ...post,
-        content,
-      },
-    },
-  };
-}
-
-export async function getStaticPaths() {
-  const posts = getAllPosts(['slug']);
-
-  return {
-    paths: posts.map(post => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      };
-    }),
-    fallback: false,
-  };
 }
