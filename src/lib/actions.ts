e9dcd89c5870f21990340727e7a081d08be9fd2e
jasontcrabtree <jasontcrabtree@gmail.com>
2024-06-08 12:58:11 +1200

    tuesday changes idk

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,30 +1,9 @@
 'use server';
 
-// export const createEntity = async (
-//   formData: FormData,
-//   entityType: EntityTypes
-// ) => {
-//   const newEntity = await fetchUtil({
-//     method: 'POST',
-//     url: `${apiEndpoint}/${entityType}`,
-//     body: {
-//       ...Object.fromEntries(formData.entries()),
-//     },
-//   });
-//   revalidatePath(`/${entityType}`, 'layout');
-//   return newEntity;
-// };
-
 import { sql } from '@vercel/postgres';
 import { RemoteBlogPost } from './types';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
-// slug: string,
-//   publishStatus: boolean,
-//   title: string,
-//   content: string,
-//   description?: string
 
 export const createNewBlogPost = async ({
   slug,
@@ -35,10 +14,12 @@ export const createNewBlogPost = async ({
 }: RemoteBlogPost) => {
   console.log('Value', content);
   try {
-    const res = await sql`
+    await sql`
         INSERT INTO "remoteblogpost" (slug, published, title, description, content)
         VALUES (${slug}, ${published}, ${title}, ${description}, ${content}) RETURNING *;`;
-    return res;
+
+    revalidatePath('/dashboard');
+    redirect('/dashboard');
   } catch (error) {
     return {
       message: `Datebase error creating new blog post: ${error}`,
